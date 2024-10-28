470c998a98f44388294f55cd81fabe75e267ae73
xhx2 <72742082+xhx2@users.noreply.github.com>
2024-10-27 20:42:16 -0700

    Add files via upload

---
+++ make_data_directories.R
@@ -0,0 +1,8 @@
+## data for raw 5-second level data
+data_path_5sec                <- 
+## directory for saving the minute level data
+data_path_processed           <- 
+## directory for ukb quality flags
+data_path_accel_quality_flags <- 
+## name of data file with data quality flags
+data_accel_quality_flags      <- 
